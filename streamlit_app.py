d395104f0389c4eef4cf681fe95e838a8a19f59c
lucyhoare <lucycwork2024@gmail.com>
2026-03-25 16:20:59 +0000

    Update streamlit_app.py
    Remove cnx from
    from snowflake.snowpark.functions import col, cnx
    
    Add line conn = cnx

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages.
 import streamlit as st
-from snowflake.snowpark.functions import col, cnx
+from snowflake.snowpark.functions import col
 
 # Write directly to the app.
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -12,6 +12,7 @@ import streamlit as st
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
+conn = cnx
 cnx = st.connection("snowflake")
 session = cnx_session()
 
