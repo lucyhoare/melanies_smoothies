7ca6ac059e71044a2082b3e94943e4b9908be82f
lucyhoare <lucycwork2024@gmail.com>
2026-03-26 09:57:32 +0000

    Update streamlit_app.py
    Amend cnx_session to cnx.session. Remove #
    cnx = st.connection("snowflake")
    session = cnx_session()
    
    Delete
    conn = st.connection("snowflake")
    session = conn_session()

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,9 @@ import streamlit as st
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
-conn = st.connection("snowflake")
-session = conn_session()
-# cnx = st.connection("snowflake")
-# session = cnx_session()
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
